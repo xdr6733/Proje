e375c5652c40ff157c707d45647b66e4a8d20906
xdr6733 <xdr6733@gmail.com>
2025-05-23 20:10:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # herkese açık hale getirir
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"status": "Uygulama Render’da çalışıyor!"}), 200
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify({"message": "Merhaba, bu herkese açık bir API!"})
 
 if __name__ == '__main__':
-    app.run()
+    # Lokal geliştirme için (Render zaten gunicorn’dan dinleyecek)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
